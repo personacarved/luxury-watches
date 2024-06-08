1022f30ed98fbe9a0313a3d043168171458578be
personacarved <152059913+personacarved@users.noreply.github.com>
2024-06-08 06:09:40 +0530

    Add files via upload

---
+++ run.js
@@ -0,0 +1,27 @@
+
+console.log('');
+console.log('');
+console.log('');
+console.log('');
+console.log('');
+console.log('');
+console.log('');
+console.log('');
+console.log('');
+console.log('');
+console.log('');
+console.log('');
+console.log('');
+console.log('');
+console.log('');
+
+
+setTimeout(function() {
+    console.log("Your React App is hosted successfully:");
+    console.log("On Network:    https://zaniswatches.rf.gd");
+}, 3000);
+
+
+setTimeout(function() {
+    console.log("");
+}, 90000);
